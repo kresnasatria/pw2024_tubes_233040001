610f1df34d68245680b1083f5bbd2b9e0816b109
kresnasatria <kresnasatria160@gmail.com>
2024-05-28 21:08:50 +0700

    booking

--- booking.php
+++ booking.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Registrasi</title>
+    <title>Halaman Booking</title>
     <link rel="stylesheet" href="css/booking.css">
 </head>
 <body>
