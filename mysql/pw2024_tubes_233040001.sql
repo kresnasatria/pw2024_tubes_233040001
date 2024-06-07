8dbf338fb820ad18dc5b829f9f0c9bfce0f05963
kresnasatria <kresnasatria160@gmail.com>
2024-06-07 22:18:53 +0700

    sql

--- mysql/pw2024_tubes_233040001.sql
+++ mysql/pw2024_tubes_233040001.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 13, 2024 at 07:49 AM
+-- Generation Time: Jun 07, 2024 at 05:17 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,56 @@ SET time_zone = "+00:00";
 --
 -- Database: `pw2024_tubes_233040001`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `booking`
+--
+
+INSERT INTO `booking` (`id`, `name`, `email`, `phone_number`, `address`, `class`) VALUES
+(1, 'kresna', 'kresnasatria@gmail.com', '123', 'antapani', 'visual effects'),
+(2, 'moreno', 'moreno@gmail.com', '123', 'antapani', 'visual effects'),
+(15, 'dara', 'dara@gmail.com', '085797385431', 'Cianjur', 'visual effects');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guru`
+--
+
+CREATE TABLE `guru` (
+  `id` int(11) NOT NULL,
+  `nama_guru` varchar(255) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `no_hp` char(15) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `kelas` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `guru`
+--
+
+INSERT INTO `guru` (`id`, `nama_guru`, `gambar`, `description`, `no_hp`, `email`, `kelas`) VALUES
+(1, 'Jack Gussion', 'visual.jpg', 'Our teacher will teach visual effects material using after effects software', '081915790124', 'jackgussion@gmail.com', 'Visual Effects'),
+(2, 'Maria Rein', 'videography.avif', 'Our teacher will teach material about videography. We use Premiere Pro.', '082264328745', 'mariarein@gmail.com', 'Videography'),
+(3, 'Ally Gill', 'photography.jpg', 'Our teacher will teach Commercial Advertising photography', '085734567546', 'allygill@gmail.com', 'Photography'),
+(4, 'Andrew Cray', 'sound.jpg', 'Our teacher will teach you the effective sound design process', '081248792587', 'andrewcray@gmail.com', 'Sound Design');
 
 -- --------------------------------------------------------
 
@@ -63,9 +113,43 @@ INSERT INTO `user` (`id`, `gambar`, `username`, `email`, `password`, `id_role`) 
 (1, '\'\'', 'kresna', 'kresnasatria160@gmail.com', 'pisanggoreng', 1),
 (2, '\'\'', 'sachrul', 'sachrul@gmail.com', '12345', 2);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_form`
+--
+
+CREATE TABLE `user_form` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `user_type` varchar(255) NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_form`
+--
+
+INSERT INTO `user_form` (`id`, `name`, `email`, `password`, `user_type`) VALUES
+(1, 'kresna', 'kresnasatria@gmail.com', '3014d92e34044334f3dea949a7763a34', 'admin'),
+(2, 'dara', 'dara@gmail.com', '3014d92e34044334f3dea949a7763a34', 'user');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `guru`
+--
+ALTER TABLE `guru`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `role`
@@ -79,6 +163,34 @@ ALTER TABLE `role`
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_role` (`id_role`);
+
+--
+-- Indexes for table `user_form`
+--
+ALTER TABLE `user_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `guru`
+--
+ALTER TABLE `guru`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user_form`
+--
+ALTER TABLE `user_form`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
